8838b56b2a02924280805996d848582b38cd97c3
unknown <bajpaisneha31@gmail.com>
2023-04-01 22:05:28 +0530

    feat: for practice

---
+++ ispostback/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ispostback
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                AddListBoxItems();
+            }
+            else
+            {
+                Response.Write("this is sneha");
+            }
+        }
+        void AddListBoxItems()
+        {
+            ListBox1.Items.Add("item1");
+        }
+    }
+}
